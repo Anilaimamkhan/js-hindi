8ee47365ab6b90ff4c80bb75be824f63db3a1212
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2024-10-04 05:21:16 +0000

    dytaType

---
+++ o1_basic/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict" // treat all ja code new version
+
+
+alert(3 + 3)
+let name = "anila";
+let age = 20;
+let isLoggedIn = false;
+
+
+// primitive dataType
+//number
+//bolen
+//string
+//undifine  (type of undefine)
+//symbol  (unique)
+//bigint
+//null   (type of object)
+
+// non primitive dataType
+//object
+//array
