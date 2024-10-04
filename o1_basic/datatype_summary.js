a93910f6176a4b5db57d65837ad907e7879f2ec6
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2024-10-04 07:10:22 +0000

    datatypeSummary

---
+++ o1_basic/datatype_summary.js
@@ -0,0 +1,40 @@
+// premitive  type k and non premitive data type
+
+// premitive 7 type k hai 
+// string
+// number
+// bollean
+// undefined
+// null
+// symbol
+// bigint
+
+// non premitive
+// array
+// object
+//function
+
+// symbol
+const id = Symbol("123");
+const anotherId = Symbol("123");
+// console.log(id);
+// console.log(id === anotherId);
+// console.log(id == anotherId);
+// const bigNumber = 1256236713150998979n
+
+
+// ********* ARRAY **************
+const names = ["anila", "komal", "naz", "fabiha"]
+let obj = {
+    name: "anila",
+    age :20,
+    qualificaion: "enter"
+}
+
+// console.log(typeof (obj))
+
+let fun = function(){
+    console.log("hello")
+}
+
+console.log(typeof(fun));
