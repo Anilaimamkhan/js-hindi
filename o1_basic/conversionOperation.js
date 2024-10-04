cf3dec06c2d28b0639d1d85e92ead1631077afdd
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2024-10-04 05:51:34 +0000

    conversionOperation

---
+++ o1_basic/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33";
+let converNumber = Number(score)
+// console.log(typeof(score))
+// console.log(typeof(converNumber))
+
+
+// "33" ko convert krengy number(score) result= number
+// "33abc" ko convert krengy number(score) result= NaN lkn type number hhi hoga
+
+let isLoggedIn = "anila";
+let boolean = Boolean(isLoggedIn)
+// console.log(boolean)
+
+// agr hum "" likhengy to false aega agr hum "anila" likhengy to true aega
+
+// CONVERT STRING
+let stringNumber = 33;
+let convertStr = String(stringNumber)
+console.log(convertStr)
+console.log(typeof convertStr)
