2ad71995e4bda6e6fc79af4d289d36d1abd8e172
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2024-10-04 06:38:40 +0000

    conversion Operation Extra Add

--- o1_basic/conversionOperation.js
+++ o1_basic/conversionOperation.js
@@ -16,5 +16,44 @@ let boolean = Boolean(isLoggedIn)
 // CONVERT STRING
 let stringNumber = 33;
 let convertStr = String(stringNumber)
-console.log(convertStr)
-console.log(typeof convertStr)
+// console.log(convertStr)
+// console.log(typeof convertStr)
+
+
+
+// ********** operation **************
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+// console.log(2**2)
+// console.log(2**3)
+// console.log(2%3)
+
+let a = "anila";
+let b = "bano";
+// console.log(a.concat( b))
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2+2);
+// console.log(1+2+"2");
+
+
+// prefix phele add hoti hai post fix ad me add hoti hai
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter)
+
+// ******* comparision oreartor **********
+// console.log(2 != 1)
+// console.log(2 == 1)
+// console.log("2" > 1);
+// console.log(null<0)
+// console.log(null > 0);
+// console.log(null == 0)
+// console.log(null <= 0)
+// console.log(null >= 0)
+console.log(undefined == 0)
+
+// undefine 0 se bara ho ya 0 se chota ho ya o k brabr ho sb me false show hogi
+
