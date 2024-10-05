ee39af759216ffd3e638765cf5f51ba92e82f5ed
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2024-10-05 14:41:46 +0000

    scope

---
+++ o1_basic/scope.js
@@ -0,0 +1,16 @@
+
+var b = 20;
+let a = 10;
+const c = 40;
+if (true) {
+    var b = 10;
+    let a =20
+    // let c = 30
+   function innner(){
+    let a = "inner anila"
+    console.log(c)
+}
+   innner()
+}
+
+// console.log(b)
