aeafb9af874baa586cabf01d2aee3bc47297189f
Anilaimamkhan <124345160+Anilaimamkhan@users.noreply.github.com>
2024-10-05 06:21:09 +0000

    date

---
+++ o1_basic/date.js
@@ -0,0 +1,29 @@
+// let myDate = new Date()
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+
+
+// let createDate = new Date(2024,10,5);
+// console.log(createDate)
+// console.log(createDate.toLocaleString())
+// console.log(createDate.getDate())
+// console.log(createDate.getTime())
+
+// let timeStamp = Date.now();
+// console.log(Math.floor(Date.now()/1000))
+
+let myDate = new Date()
+myDate.toLocaleString("default",{
+weekday : "narrow"
+})
+console.log(myDate)
+// console.log(myDate.getDate());
+// console.log(myDate.getTime());
+// console.log(myDate.getMonth()+1);
+// console.log(myDate.getMinutes());
+
+
+
+
+
